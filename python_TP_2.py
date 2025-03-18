6e44a6ee90ea9e48f26708ee168e0f51c22f60b1
E.White <97997547+WhitedMos@users.noreply.github.com>
2025-03-18 00:26:46 +0000

    updated

---
+++ python_TP_2.py
@@ -0,0 +1,24 @@
+# Exercice 1
+print("\nExercice 1")
+x = True
+y = int (x)
+print(y)
+print(f"1.1: {type(x)}, {type(y)}")
+
+x = False
+y = int (x)
+print(y)
+print(f"1.2: {type(x)}, {type(y)}")
+
+print("1.3: Il convertit True en 1 et False en 0")
+
+x = False
+y = True
+print(x + x)
+print(x + y)
+print(f"1.4: {type(x)}, {type(y)}")
+
+print("1.5: Il convertit False en 0 et True en 1, puis fait l'addition")
+
+# Exercice 2
+print("\nExercice 2")
