7b208fb3be79c98fd9cc053067a9b607050143b0
E.White <97997547+WhitedMos@users.noreply.github.com>
2025-03-18 00:48:49 +0000

    updated

--- python_TP_2.py
+++ python_TP_2.py
@@ -1,3 +1,11 @@
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("-" * 20)
+        result = func(*args, **kwargs)
+        print("-" * 20)
+        return result
+    return wrapper
+
 # Exercice 1
 print("\nExercice 1")
 x = True
@@ -22,3 +30,34 @@ print("1.5: Il convertit False en 0 et True en 1, puis fait l'addition")
 
 # Exercice 2
 print("\nExercice 2")
+x = 2
+y = 5
+print(max(x, y))
+print(min(x, y))
+print("2.1: La commande max() renvoie le plus grand nombre, min() le plus petit")
+
+print("2.2:")
+x = input("x")
+y = input("y")
+max_input = max(x,y)
+if max_input == x:
+    print(1)
+else:
+    print(0)
+
+print("2.3:")
+if input("x") >= input("y"):
+    print("maximal est x")
+else:
+    print("maximal est y")
+
+print("2.4:")
+if input("x") >= input("y"):
+    print("minimal est y")
+else:
+    print("minimal est x")
+
+# Exercice 3
+print("\nExercice 3")
+def xor(a, b):
+    return a != b
